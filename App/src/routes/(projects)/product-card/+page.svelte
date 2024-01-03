75720dedf301e015ca581332dab871ec531cf444
CedricBorko <cedric@borkoland.de>
2024-01-03 09:48:48 +0100

    Project Card

---
+++ App/src/routes/(projects)/product-card/+page.svelte
@@ -0,0 +1,36 @@
+<div class="p-4 bg-orange-200/40">
+	<div class="rounded-lg overflow-hidden bg-white sm:grid sm:grid-cols-2 max-w-screen-sm mx-auto">
+		<div>
+			<img
+				src="src/lib/projects/Product Card/images/image-product-mobile.jpg"
+				alt="Chanel Parfum Bottle"
+				class="sm:hidden block"
+			/>
+			<img
+				src="src/lib/projects/Product Card/images/image-product-desktop.jpg"
+				alt="Chanel Parfum Bottle"
+				class="hidden sm:block"
+			/>
+		</div>
+		<div class="space-y-4 p-4 md:p-8">
+			<div class="space-y-2">
+				<p class="tracking-[0.25em] uppercase text-gray-400">Perfume</p>
+				<h1 class="font-bold text-4xl max-w-[15ch]">Gabrielle Essence Eau De Parfum</h1>
+			</div>
+			<p class="text-gray-500 leading-relaxed">
+				A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
+				for the House of CHANEL.
+			</p>
+
+			<div class="flex items-center gap-4">
+				<p class="text-green-800/80 font-bold text-4xl">$149.99</p>
+				<p class="text-gray-500">$169.99</p>
+			</div>
+			<button
+				class="w-full bg-green-800/80 hover:opacity-90 transition-opacity text-white font-bold py-3 rounded-lg"
+			>
+				Add to Cart
+			</button>
+		</div>
+	</div>
+</div>
---
+++ App/src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-center p-4 text-5xl font-bold">Hello World</h1>
